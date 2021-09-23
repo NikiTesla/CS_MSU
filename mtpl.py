b51c7b31f3eeff52caa530d1fc8c30ad09e1cdd5
Nikita <krechetov.n@outlook.com>
2021-09-23 22:15:38 +0300

    commit

--- mtpl.py
+++ mtpl.py
@@ -1,10 +1,11 @@
+# %%
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.arange(-100, 100, 1)
-y = x ** 3
+x = np.arange(- 2 * np.pi,2 * np.pi,  np.pi/10)
+y = np.sin(x)
 
 print(y)
 plt.plot(x, y,  '-r')
-plt.show()
-
+plt.show()  
+# %%
